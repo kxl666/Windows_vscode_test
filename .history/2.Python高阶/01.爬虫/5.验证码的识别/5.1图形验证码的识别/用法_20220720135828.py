fbd31e585f6682f8b6af369a124430a515e9d7e2
kxl <2244951869@qq.com>
2022-07-20 14:01:00 +0800

    7/20

--- .history/2.Python高阶/01.爬虫/5.验证码的识别/5.1图形验证码的识别/用法_20220720135828.py
+++ .history/2.Python高阶/01.爬虫/5.验证码的识别/5.1图形验证码的识别/用法_20220720135828.py
@@ -11,14 +11,14 @@ with open(
 image = Image.open(
     r'C:\Users\kxl666\Desktop\Python\2.Python高阶\01.爬虫\5.验证码的识别\5.1图形验证码的识别\code.jpg'
 )
-image = image.convert('L')  # 转换为灰度图
-threshold = 127  # 阈值
-table = []  # 定义一个存放转换后的像素值的列表
+image = image.convert('L')
+threshold = 127
+table = []
 for i in range(256):
-    if i < threshold:  # 小于阈值时，设置为0
-        table.append(0)  # 小于阈值时，设置为0
+    if i < threshold:
+        table.append(0)
     else:
-        table.append(1)  # 大于阈值时，设置为1
-image = image.point(table, '1')  # 转换为二值图
-result = pytesseract.image_to_string(image)  # 识别图片
+        table.append(1)
+image = image.point(table, '1')
+result = pytesseract.image_to_string(image)
 print(result)
